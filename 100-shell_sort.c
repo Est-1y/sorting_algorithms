fd3e234e772799822497a6158763f57294010f62
Est-1y <terryiana22@gmail.com>
2023-11-18 04:53:21 -0800

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swaps two integers in an array
+ * @a: first integer to be swapped
+ * @b: second integer to be swapped
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * shell_sort - this Sorts arrays of integers in ascending
+ * order using the shell sort algorithm
+ * @array: array of integers
+ * @size: size of the array
+ * Description: it uses the Knuth interval sequence
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
