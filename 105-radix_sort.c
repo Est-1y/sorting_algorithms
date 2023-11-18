7d19f671b23788f45a5861dc7fb2959e847542db
Est-1y <terryiana22@gmail.com>
2023-11-18 05:36:34 -0800

    radix sort

---
+++ 105-radix_sort.c
@@ -0,0 +1,82 @@
+#include "sort.h"
+
+int get_max(int *array, int size);
+void radix_counting_sort(int *array, size_t size, int sig, int *buff);
+void radix_sort(int *array, size_t size);
+
+/**
+ * get_max - Gets maximum value in an array of integers
+ * @array: array of integers
+ * @size: size of the array
+ * Return: maximum integer in the array
+ */
+int get_max(int *array, int size)
+{
+	int max, i;
+
+	for (max = array[0], i = 1; i < size; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+
+	return (max);
+}
+
+/**
+ * radix_counting_sort - Sorts significant digits of arrays of integers
+ * in ascending order using the counting sort algorithm
+ * @array: array of integers
+ * @size: size of the array
+ * @sig: significant digit to be sorted on
+ * @buff: buffer storing the sorted array
+ */
+void radix_counting_sort(int *array, size_t size, int sig, int *buff)
+{
+	int count[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		count[(array[i] / sig) % 10] += 1;
+
+	for (i = 0; i < 10; i++)
+		count[i] += count[i - 1];
+
+	for (i = size - 1; (int)i >= 0; i--)
+	{
+		buff[count[(array[i] / sig) % 10] - 1] = array[i];
+		count[(array[i] / sig) % 10] -= 1;
+	}
+
+	for (i = 0; i < size; i++)
+		array[i] = buff[i];
+}
+
+/**
+ * radix_sort - Sorts arrays of integers in ascending
+ * order using the radix sort algorithm
+ * @array:array of integers
+ * @size: size of the array
+ * Description: Executes the LSD radix sort algorithm and
+ * Prints the array after each significant digit increase.
+ */
+void radix_sort(int *array, size_t size)
+{
+	int max, sig, *buff;
+
+	if (array == NULL || size < 2)
+		return;
+
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
+		return;
+
+	max = get_max(array, size);
+	for (sig = 1; max / sig > 0; sig *= 10)
+	{
+		radix_counting_sort(array, size, sig, buff);
+		print_array(array, size);
+	}
+
+	free(buff);
+}
